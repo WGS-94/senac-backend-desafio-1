2b43980a05d470524143fc04e1a662ce61add5d8
WGS-94 <wilson-senx@hotmail.com>
2024-11-04 16:42:26 -0300

    :tada: Definindo uma rota para criar um novo livro

--- app.js
+++ app.js
@@ -12,6 +12,36 @@ app.use(express.json());
 // Declarar um array vazio para armazenar os livros
 const books = [];
 
+// ROUTAS
+
+// Definindo uma rota para criar um novo livro
+app.post("/api/cadastrar-livro", (request, response) => {
+
+  // Receber as informações do livro no body da requisição
+  const { title, description, authors, ISBN, categories, rating } = request.body;
+
+  // Verifica se o livro já existe de acordo ao parâmetro ISBN, por ser único
+  const bookAlreadyExists = books.some((book) => book.ISBN === ISBN);
+
+  if (bookAlreadyExists) {
+    return response.status(400).json({ error: "Book already exists!" });
+  }
+
+  // Armazena o livro no array
+  books.push({
+    id: uuidv4(),
+    title,
+    description,
+    authors,
+    ISBN,
+    categories,
+    rating,
+    createdAt: new Date()
+  });
+
+  return response.status(201).send(books);
+});
+
 // Iniciar o servidor para ouvir na porta 5000
 app.listen(5000, () => {
   console.log('Servidor rodando na porta 5000...');
