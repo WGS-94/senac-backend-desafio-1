ba8dcca6e2b2ab578023daeda3b8d3c43c979436
WGS-94 <wilson-senx@hotmail.com>
2024-11-04 16:41:22 -0300

    :sparkles: Declarar um array vazio para armazenar os livros e inciar aplicação na porta 5000

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ const app = express();
 
 //  Definir o tipo de conteúdo que será retornado pela aplicação
 app.use(express.json());
+
+// Declarar um array vazio para armazenar os livros
+const books = [];
+
+// Iniciar o servidor para ouvir na porta 5000
+app.listen(5000, () => {
+  console.log('Servidor rodando na porta 5000...');
+});
